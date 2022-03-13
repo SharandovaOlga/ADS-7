9390ff34f3be4c03c9c8af1e22864039e284f2d2
SharandovaOlga <75723905+SharandovaOlga@users.noreply.github.com>
2022-03-13 14:27:53 +0300

    ADS-1 Sharandova

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,61 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  bool a = true;
+    for (int j = 1; j < value; j++) {
+      if ((value % j == 0) && (j != 1)) {
+        a = false;
+        break;
+      }  
+    }
+    return (a);
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  int b = 1;
+  int y = 0;
+  int j = 2;
+  while (y < n) {
+    bool a = true;
+    for (int sr = 2; sr < j; sr++) {
+      if (j %r sr == 0) {
+        a = false;
+        break;
+      }
+    }
+      if (a) {
+        b = j;
+        y++;
+      }
+      j++;
+  }
+  return(b);
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  int sr = value;
+  while (true) {
+    bool a = true;
+    sr++;
+    for (int j = 2; j< sr; j++) {
+      if (sr % j == 0){
+        a = false;
+        break;
+      }
+    }
+    if (a) {
+      return (sr);
+      break;
+    }
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t sum = 0;
+  for (uint64_t j = 2; j < hbound; j++) {
+    if (checkPrime(j)) {
+      sum += j;
+    }
+  }
+  return sum;
 }
